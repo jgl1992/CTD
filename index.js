5a0c4c2dfe3c8c1d82b9c43d5ebbbcf870f59eaf
jgl1992 <70378865+jgl1992@users.noreply.github.com>
2025-08-16 11:52:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const apiURL = "https://open-meteo.com/";
 
+try {
+  const response = await fetch(apiURL);
+  if(!response.ok) {
+    throw new Error('http error! status: ${response.status}');
+  }
+  
+const data = await response.json(); // or response.text()
+  console.log('collected data: ', data);
+  return data; // return the data if needed
+} catch (Error) {
+  console.error ('fetch error:', error);
+}
+fetchdata from api();
+
+   
